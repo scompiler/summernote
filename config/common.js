fda1a921814dbb1d20c1b3dbe8e657f5791569ae
Kos <kostyaveselovsky@gmail.com>
2022-10-09 15:02:00 +0300

    feat: remove bs3 style

--- config/common.js
+++ config/common.js
@@ -2,20 +2,20 @@ const { readdirSync, readFileSync } = require('fs');
 const glob = require('glob');
 const path = require('path');
 
-const defaultStyle = 'bs3';
+const defaultStyle = 'teaks2';
 
 module.exports = {
   defaultStyle,
   // Styles in /src/styles
   // Each entity becomes an entry point in Webpack build process.
-  styles: 
+  styles:
     readdirSync('./src/styles', { withFileTypes: true })
       .filter(dirent => dirent.isDirectory() && dirent.name != 'summernote')
-      .map(dirent => { 
+      .map(dirent => {
         const id = dirent.name;
         const target = id === defaultStyle ? 'summernote' : `summernote-${id}`;
         let name;
-        
+
         // Try to find information json file and get them.
         try {
           let info = JSON.parse(readFileSync(`./src/styles/${id}/summernote-${id}.json`));
@@ -35,10 +35,10 @@ module.exports = {
   examples:
     glob.sync('./examples/*.html')
       .filter(f => f.charAt(0) != '_') // except files start with underscore
-      .map(filepath => { 
+      .map(filepath => {
         let title;
         let name = path.parse(filepath).name;
-        
+
         // Try to parse html and get <title> from it.
         try {
           let html = readFileSync(filepath).toString();
