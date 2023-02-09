31e9ddf914f3cd17823171ebf809232aec4c9b9c
Kos <kostyaveselovsky@gmail.com>
2023-02-09 13:01:26 +0300

    feat: rewrite test/base/module/Fullscreen.spec.js to typescript

---
+++ test/base/module/Fullscreen.spec.ts
@@ -0,0 +1,32 @@
+/**
+ * Fullscreen.spec.js
+ * (c) 2015~ Summernote Team
+ * summernote may be freely distributed under the MIT license./
+ */
+
+import chai from 'chai';
+import $ from 'jquery';
+import Context from 'src/js/Context';
+import Fullscreen from 'src/js/module/Fullscreen';
+import 'src/styles/bs4/summernote-bs4';
+import Summernote from "../../../src/js/class";
+
+describe('Fullscreen', () => {
+    const expect = chai.expect;
+    let fullscreen: Fullscreen;
+    let context: Context;
+
+    beforeEach(() => {
+        const options = $.extend({}, Summernote.meta.options);
+        context = new Context($('<div><p>hello</p></div>')[0], options);
+        fullscreen = new Fullscreen(context);
+    });
+
+    it('should toggle fullscreen mode', () => {
+        expect(fullscreen.isFullscreen()).to.be.false;
+        fullscreen.toggle();
+        expect(fullscreen.isFullscreen()).to.be.true;
+        fullscreen.toggle();
+        expect(fullscreen.isFullscreen()).to.be.false;
+    });
+});
